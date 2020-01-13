582a35520cc93b978a34ab42a954596c701e7e45
msimon42 <msimon@uab.edu>
2020-01-13 10:26:46 -0700

    Set up flight passenger stats

--- spec/features/flights/show_spec.rb
+++ spec/features/flights/show_spec.rb
@@ -5,13 +5,14 @@ RSpec.describe 'When i visit a flight show page' do
     @airline = Airline.create(name: "United")
     @flight = Flight.create(number: "569", date: "10/10/20", time: "1300", departure_city: "Denver", arrival_city: "San Francisco", airline: @airline)
     @passenger_1 = @flight.passengers.create(name: 'Harry', age: 25)
+    @passenger_4 = @flight.passengers.create(name: 'Bob', age: 35)
     @passenger_2 = @flight.passengers.create(name: 'Dwight', age: 16)
     @passenger_3 = Passenger.create(name: 'Draco', age: 23)
+
+    visit "/flights/#{@flight.id}"
   end
 
   it 'I see all information pertaining to that flight' do
-    visit "/flights/#{@flight.id}"
-
     expect(page).to have_content(@flight.number)
     expect(page).to have_content(@flight.date)
     expect(page).to have_content(@flight.time)
@@ -19,7 +20,13 @@ RSpec.describe 'When i visit a flight show page' do
     expect(page).to have_content(@flight.arrival_city)
     expect(page).to have_content(@flight.airline.name)
     expect(page).to have_content(@passenger_1.name)
+    expect(page).to have_content(@passenger_4.name)
     expect(page).to have_content(@passenger_2.name)
     expect(page).to_not have_content(@passenger_3.name)
+  end
+
+  it 'I see number of adults and minors on flight' do
+    expect(page).to have_content("Adults: 2")
+    expect(page).to have_content("Minors: 1")
   end
 end
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -14,4 +14,22 @@ RSpec.describe Flight, type: :model do
     it {should have_many :passenger_flights}
     it {should have_many(:passengers).through(:passenger_flights)}
   end
+
+  describe 'methods' do
+    before :each do
+      @airline = Airline.create(name: "United")
+      @flight = Flight.create(number: "569", date: "10/10/20", time: "1300", departure_city: "Denver", arrival_city: "San Francisco", airline: @airline)
+      @passenger_1 = @flight.passengers.create(name: 'Harry', age: 25)
+      @passenger_2 = @flight.passengers.create(name: 'Dwight', age: 16)
+      @passenger_3 = @flight.passengers.create(name: 'Draco', age: 23)
+    end
+
+    it 'adult_count' do
+      expect(@flight.adult_count).to eq(2)
+    end
+
+    it 'minor_count' do
+      expect(@flight.minor_count).to eq(1)
+    end
+  end
 end
